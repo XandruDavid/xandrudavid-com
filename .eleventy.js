02c9cc89689e1ff3823703ab1e3128c8d3fd86e0
Alexandru David <xandru.david@gmail.com>
2024-08-31 23:58:28 +0200

    feat: add some basic content, style, and fonts

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ "src/styles/reset.css": "reset.css" });
+  eleventyConfig.addPassthroughCopy({ "src/styles/": "./" });
+  eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Regular.woff2": "./" });
+  eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Medium.woff2": "./" });
+  eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Bold.woff2": "./" });
 
   eleventyConfig.addWatchTarget("src/styles");
 
