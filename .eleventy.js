0806c615024eccf5598a2269d993c604eb8cfb6f
Alexandru David <xandru.david@gmail.com>
2025-10-31 21:45:50 +0100

    fix: build passthrough paths

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ "src/styles/": "./" });
-  eleventyConfig.addPassthroughCopy({ "src/assets/": "./" });
-  eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Light.woff2": "./" });
-  eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Regular.woff2": "./" });
-  eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Medium.woff2": "./" });
-  eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Bold.woff2": "./" });
+  eleventyConfig.addPassthroughCopy({ "src/styles": "." });
+  eleventyConfig.addPassthroughCopy({ "src/assets": "." });
+  eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Light.woff2": "HKGrotesk-Light.woff2" });
+  eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Regular.woff2": "HKGrotesk-Regular.woff2" });
+  eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Medium.woff2": "HKGrotesk-Medium.woff2" });
+  eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Bold.woff2": "HKGrotesk-Bold.woff2" });
 
   eleventyConfig.addWatchTarget("src/styles");
 
