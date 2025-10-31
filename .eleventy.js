3e7dbe066b2f8d6cd7f4fdf558af7592082c4b54
Alexandru David <xandru.david@gmail.com>
2025-10-31 16:00:46 +0100

    feat: add local font and enrich employment section

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/styles/": "./" });
   eleventyConfig.addPassthroughCopy({ "src/assets/": "./" });
+  eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Light.woff2": "./" });
   eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Regular.woff2": "./" });
   eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Medium.woff2": "./" });
   eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Bold.woff2": "./" });
