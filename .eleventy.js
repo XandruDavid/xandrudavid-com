82528acb2f4a2e3665f2016c6328f807075f7015
Alexandru David <xandru.david@gmail.com>
2025-10-31 22:46:47 +0100

    chore: improve SEO with meta tags, robots and sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Regular.woff2": "HKGrotesk-Regular.woff2" });
   eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Medium.woff2": "HKGrotesk-Medium.woff2" });
   eleventyConfig.addPassthroughCopy({ "src/fonts/hk-grotesk/HKGrotesk-Bold.woff2": "HKGrotesk-Bold.woff2" });
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/sitemap.xml");
 
   eleventyConfig.addWatchTarget("src/styles");
 
